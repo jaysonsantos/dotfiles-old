f21bd93d95c5abe9592f69d92686b3147c1799e7
Jayson Reis <santosdosreis@gmail.com>
2011-10-25 22:43:05 -0200

    added django version

--- ps1.sh
+++ ps1.sh
@@ -8,9 +8,14 @@ function python_version() {
    echo -n "" $(python --version &> /dev/stdout)
 }
 
+function django_version() {
+   echo -ne '\033[0;32m'
+   echo -n ' Django' $(django-admin.py --version)
+}
+
 function linebreak() {
    echo -e '\033[00m\n$ ';
 }
 
 export PS1="\[\e]0;\u@\h: \w\a\\u@\h:\w"
-export PS1="$PS1\`git_branch\`\`python_version\`\`linebreak\`"
+export PS1="$PS1\`git_branch\`\`python_version\`\`django_version\`\`linebreak\`"
