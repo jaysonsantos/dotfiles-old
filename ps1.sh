775288852f2161843349c7dd4197fc19d03bd734
Jayson Reis <santosdosreis@gmail.com>
2011-12-18 23:16:16 -0200

    some changes

--- ps1.sh
+++ ps1.sh
@@ -1,20 +1,24 @@
 function git_branch() {
-   echo -ne '\033[0;33m'
-   echo -n "" $(git branch &> /dev/stdout|grep ^\*|sed 's/\* \(.*\)/ \(\1\) /')
+    echo -ne '\033[0;33m'
+    echo -n "" $(git branch &> /dev/stdout|grep ^\*|sed 's/\* \(.*\)/ \(\1\) /')
 }
 
 function python_version() {
-   echo -ne '\033[0;31m'
-   echo -n "" $(python --version &> /dev/stdout)
+    echo -ne '\033[0;31m'
+    echo -n "" $(python --version &> /dev/stdout)
 }
 
 function django_version() {
-   echo -ne '\033[0;32m'
-   echo -n ' Django' $(django-admin.py --version)
+    if [ -z "$(which django-admin.py)" ]
+    then
+        return
+    fi
+    echo -ne '\033[0;32m'
+    echo -n ' Django' $(django-admin.py --version)
 }
 
 function linebreak() {
-   echo -e '\033[00m\n$ ';
+    echo -e '\033[00m\n$ ';
 }
 
 export PS1="\[\e]0;\u@\h: \w\a\\u@\h:\w"
