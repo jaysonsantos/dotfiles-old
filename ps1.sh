913dc22893f3d601bab2203a97a464c2f32c6565
Erick Wilder <erickwilder@gmail.com>
2012-08-27 15:39:21 -0300

    Refatoração e reuso de verificação de copia de trabalho e modificação da verificação de cópia suja

--- ps1.sh
+++ ps1.sh
@@ -3,36 +3,51 @@ c_red=`tput setaf 1`
 c_green=`tput setaf 2`
 c_sgr0=`tput sgr0`
 
-function branch_is_clean()
+
+function is_git_working_copy()
 {
-    is_clean=0
+    wc=0
     if git rev-parse --git-dir >/dev/null 2>&1
     then
-        if git diff --quiet 2>/dev/null >&2
-            is_clean=0
-        then
-            is_clean=1
-        fi
+        wc=0
+    else
+        wc=1
     fi
 
-    return $is_clean
+    return $wc
+}
+
+function branch_is_dirty()
+{
+    is_dirty=0
+    if is_git_working_copy
+    then
+        is_dirty=$(git status | grep clean | wc -l)
+    fi
+
+    return $is_dirty
 }
 
 function branch_color ()
 {
     color=""
-    if branch_is_clean
+    if is_git_working_copy
     then
-        color=${c_green}
+        if ! branch_is_dirty
+        then
+            color=${c_green}
+        else
+            color=${c_red}
+        fi
     else
-        color=${c_red}
+        return 0
     fi
     echo -ne $color
 }
 
 function git_branch ()
 {
-    if git rev-parse --git-dir >/dev/null 2>&1
+    if is_git_working_copy
     then
         gitver=$(git branch 2>/dev/null| sed -n '/^\*/s/^\* //p')
     else
@@ -40,7 +55,7 @@ function git_branch ()
     fi
     echo -ne " ("$gitver
 
-    if ! branch_is_clean
+    if branch_is_dirty
     then
          echo -n "*"
     fi
